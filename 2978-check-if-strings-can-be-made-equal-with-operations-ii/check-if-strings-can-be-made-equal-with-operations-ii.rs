bb90789b6f6d409ad488786d6e9ca0f1f6fa17a2
Swarnarup <93971543+Swarnarup@users.noreply.github.com>
2026-03-30 19:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 2.5 MB (90.91%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.rs
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.rs
@@ -1,21 +1,44 @@
+// impl Solution {
+//     pub fn check_strings(s1: String, s2: String) -> bool {
+//         let mut v10: Vec<char> = Vec::new();
+//         let mut v11: Vec<char> = Vec::new();
+//         for (i, ch) in s1.chars().enumerate(){
+//             if i&1 == 0 { v10.push(ch); }
+//             else { v11.push(ch); }
+//         }
+//         v10.sort();
+//         v11.sort();
+//         let mut v20: Vec<char> = Vec::new();
+//         let mut v21: Vec<char> = Vec::new();
+//         for (i, ch) in s2.chars().enumerate(){
+//             if i&1 == 0 { v20.push(ch); }
+//             else { v21.push(ch); }
+//         }
+//         v20.sort();
+//         v21.sort();
+//         v10 == v20 && v11 == v21
+//     }
+// }
+
+// frequency match
 impl Solution {
     pub fn check_strings(s1: String, s2: String) -> bool {
-        let mut v10: Vec<char> = Vec::new();
-        let mut v11: Vec<char> = Vec::new();
+        let mut f10: Vec<i32> = vec![0; 26];
+        let mut f11: Vec<i32> = vec![0; 26];
         for (i, ch) in s1.chars().enumerate(){
-            if i&1 == 0 { v10.push(ch); }
-            else { v11.push(ch); }
+            if i&1 == 0 { f10[get_idx(ch)] += 1 }
+            else { f11[get_idx(ch)] += 1 }
         }
-        v10.sort();
-        v11.sort();
-        let mut v20: Vec<char> = Vec::new();
-        let mut v21: Vec<char> = Vec::new();
+        let mut f20: Vec<i32> = vec![0; 26];
+        let mut f21: Vec<i32> = vec![0; 26];
         for (i, ch) in s2.chars().enumerate(){
-            if i&1 == 0 { v20.push(ch); }
-            else { v21.push(ch); }
+            if i&1 == 0 { f20[get_idx(ch)] += 1 }
+            else { f21[get_idx(ch)] += 1 }
         }
-        v20.sort();
-        v21.sort();
-        v10 == v20 && v11 == v21
+        f10 == f20 && f11 == f21
     }
+}
+
+fn get_idx(i: char) -> usize {
+    i as usize - 'a' as usize
 }
